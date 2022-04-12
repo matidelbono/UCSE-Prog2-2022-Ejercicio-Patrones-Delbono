aa352aa7c57b88a9acf4bc3ab4906ae07efc5d57
Mati <mati.delbono00@gmail.com>
2022-04-12 15:49:02 -0300

    Correo enviado

--- Ejercicio Patrones -1/Program.cs
+++ Ejercicio Patrones -1/Program.cs
@@ -27,9 +27,37 @@ namespace Ejercicio_Patrones__1
                 string Nombre = Console.ReadLine();
                 Console.WriteLine("Cargue el DNI ");
                 DNI= int.Parse(Console.ReadLine());
-
             }
-          
+
+            {
+                Console.WriteLine("Mail To");
+                MailAddress to = new MailAddress(Console.ReadLine());
+
+                Console.WriteLine("Mail From");
+                MailAddress from = new MailAddress(Console.ReadLine());
+
+                MailMessage mail = new MailMessage(from, to);
+
+                Console.WriteLine("Subject");
+                mail.Subject = Console.ReadLine();
+
+                Console.WriteLine("Your Message");
+                mail.Body = Console.ReadLine();
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+
+                smtp.Credentials = new NetworkCredential(
+                    "username@domain.com", "password");
+                smtp.EnableSsl = true;
+                Console.WriteLine("Sending email...");
+                smtp.Send(mail);
+            }
+
+
+
+
         }
 }
 }
